be7e8f1bcb0a54e42d503443b952f32a9b42933c
BillYJT <36845351+BillYJT@users.noreply.github.com>
2019-11-09 21:07:34 +1100

    MCU code

---
+++ DSO_2019_CODE/MCU_Firmware/DSO Project Base Code/default/dep/USBDSO.o.d
@@ -0,0 +1,36 @@
+USBDSO.o: ../USBDSO.c \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  ../USBDSO.h ../Macros.h
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/windows/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../USBDSO.h:
+
+../Macros.h:
